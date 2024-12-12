65eca345248aa42fda59f2d4d9ee02988344d5b3
Mingxuan Che <mxche2019@outlook.com>
2024-12-12 17:11:35 +0100

    update mb run script

--- benchmarking_sim/quadrotor/mb_rollout.sh
+++ benchmarking_sim/quadrotor/mb_rollout.sh
@@ -1,13 +1,13 @@
 
 
 # for STARTSEED in 1 21 41 61 81
-for STARTSEED in 40
-do 
-    for ADDITIOANL in '_11' # '_10' '_12' '_13' '_14'
-    do
-        python3 mb_experiment_rollout.py $STARTSEED 20 $ADDITIOANL 'gpmpc_acados'
-    done
-done
+# for STARTSEED in 40
+# do 
+#     for ADDITIOANL in '_11' # '_10' '_12' '_13' '_14'
+#     do
+#         python3 mb_experiment_rollout.py $STARTSEED 20 $ADDITIOANL 'gpmpc_acados'
+#     done
+# done
 
 # for algo in 'mpc_acados' # 'linear_mpc' 
 # do
@@ -19,3 +19,30 @@ done
 #         done
 #     done
 # done
+
+# for ADDITIOANL in '_9' '_11' '_13' '_15'
+# for ADDITIOANL in '_10' '_12' '_14'
+for ADDITIOANL in '_9' '_10' '_11' '_12' '_13' '_14' '_15'
+do
+    for STARTSEED in 1 11 21 31 41 51 61 71 81 91 
+    do 
+        # for algo in 'lqr' 'ilqr' # 'pid' 
+        # for algo in 'linear_mpc' 
+        for algo in 'ilqr' # 'lqr'
+        do
+            python3 results_rollout.py $ADDITIOANL $STARTSEED $algo 1
+        done
+    done
+done
+
+for ADDITIOANL in ''
+do
+    for STARTSEED in 1 
+    do 
+        for algo in 'ilqr'
+        # for algo in 'linear_mpc_acados'
+        do
+            python3 results_noise.py $algo
+        done
+    done
+done
---
+++ benchmarking_sim/quadrotor/results_rollout.py
@@ -0,0 +1,51 @@
+import numpy as np
+import os
+import sys
+import matplotlib.pyplot as plt
+import munch
+
+from benchmarking_sim.quadrotor.benchmark_util.utils import run_rollouts
+
+notebook_dir = os.path.dirname(os.path.abspath('__file__'))
+print('notebook_dir', notebook_dir)
+
+colors = plt.rcParams['axes.prop_cycle'].by_key()['color']
+s = 2 # times of std
+
+# # roll out
+# additionals_list = ['', '_slow', '_fast']
+# additionals_list = ['_'+repr(i) for i in range(9, 16)]
+# start_seed = [i for i in range(1, 100, 10)]
+# for algo in ['pid', 'lqr', 'ilqr']:
+#     for additional in additionals_list:
+#         for start_seed in start_seed:
+
+additional = sys.argv[1]
+start_seed = int(sys.argv[2])
+algo = sys.argv[3]
+# noise_factor = int(sys.argv[4])
+
+task_description = munch.munchify({
+    'additional': additional,
+    'algo': algo,
+    'eval_task': 'rollout',
+    'start_seed': start_seed,
+    })
+run_rollouts(task_description)
+
+# # noise factor test
+# additional = ''
+# noise_factor_list = np.arange(0, 200, 10)
+# noise_factor_list[0] = 1
+# for algo in ['pid', 'lqr', 'ilqr']:
+#     for noise_factor in noise_factor_list:
+#         for start_seed in range(1, 11):
+#             task_description = munch.munchify({
+#                 'additional': additional,
+#                 'algo': algo,
+#                 'noise_factor': noise_factor,
+#                 'eval_task': 'noise',
+#                 'num_seed': 1,
+#                 'start_seed': start_seed,
+#                 })
+#             run_rollouts(task_description)
