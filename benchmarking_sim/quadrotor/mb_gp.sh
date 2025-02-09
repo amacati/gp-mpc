ebb1e53ea40ee0a01ad9501262988f3106c31e47
Mingxuan Che <mxche2019@outlook.com>
2025-02-09 01:50:06 +0100

    add acados folder and config cleanup script

--- benchmarking_sim/quadrotor/mb_gp.sh
+++ benchmarking_sim/quadrotor/mb_gp.sh
@@ -1,8 +1,8 @@
 
 
-# for ADDITIOANL in '_dw_h=1dot5' '_dw_h=2dot5' '_dw_h=4'
+for ADDITIOANL in '_dw_h=1dot5' '_dw_h=2' '_dw_h=2dot5' '_dw_h=3'
 # for ADDITIOANL in '_ob_ns=5' '_ob_ns=15' '_ob_ns=25'  
-for ADDITIOANL in '_proc_ns=5' '_proc_ns=15' '_proc_ns=25'
+# for ADDITIOANL in  '_proc_ns=25' # '_proc_ns=5' '_proc_ns=15'
 # for ADDITIOANL in '_tr'
 do
     for STARTSEED in 1
@@ -12,4 +12,8 @@ do
             python3 gpmpc_experiment.py $algo $ADDITIOANL
         done
     done
+
+    # python3 results_dw.py 'gpmpc_acados_TP' $GP_TAG
 done
+
+python3 ../del_acados_files.py
