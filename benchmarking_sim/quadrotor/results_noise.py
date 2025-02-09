50f6c80f40465558b05a7d8b4fd139121f61f476
Mingxuan Che <mxche2019@outlook.com>
2025-02-09 02:37:59 +0100

    minor udpate mb rollout

--- benchmarking_sim/quadrotor/results_noise.py
+++ benchmarking_sim/quadrotor/results_noise.py
@@ -25,8 +25,8 @@ s = 2 # times of std
 # algo = sys.argv[3]
 # noise_factor = int(sys.argv[4])
 algo = sys.argv[1]
-gp_model_tag = sys.argv[2] if len(sys.argv) > 2 else ''
-noise_type = sys.argv[3] if len(sys.argv) > 3 else 'obs_noise'
+noise_type = sys.argv[2] if len(sys.argv) > 2 else 'obs_noise'
+gp_model_tag = sys.argv[3] if len(sys.argv) > 3 else ''
 # task_description = munch.munchify({
 #     'additional': additional,
 #     'algo': algo,
@@ -43,22 +43,22 @@ noise_factor_list = [0,1,2,3,4,5,10,15,20,25,\
                      30,40,50,60,70,80,90,100]
 # noise_factor_list[0] = 1
 # for algo in ['pid', 'lqr', 'ilqr']:
-if noise_type == 'obs':
-    for noise_factor in noise_factor_list:
-        for start_seed in range(1, 11):
-            task_description = munch.munchify({
-                'additional': additional,
-                'algo': algo,
-                'noise_factor': noise_factor,
-                'eval_task': noise_type,
-                'num_seed': 1,
-                'start_seed': start_seed,
-                # 'SYS': 'quadrotor_3D_attitude',
-                'SYS': 'quadrotor_2D_attitude', 
-                'gp_model_tag': gp_model_tag,
-                })
-            run_rollouts(task_description)
-        
+
+for noise_factor in noise_factor_list:
+    for start_seed in range(1, 11):
+        task_description = munch.munchify({
+            'additional': additional,
+            'algo': algo,
+            'noise_factor': noise_factor,
+            'eval_task': noise_type,
+            'num_seed': 1,
+            'start_seed': start_seed,
+            # 'SYS': 'quadrotor_3D_attitude',
+            'SYS': 'quadrotor_2D_attitude', 
+            'gp_model_tag': gp_model_tag,
+            })
+        run_rollouts(task_description)
+    
         
 # for noise_factor in noise_factor_list:
 #     for start_seed in range(1, 11):
