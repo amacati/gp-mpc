967e65004ac55919578bfd8e89c1a600c0f64e7e
Mingxuan Che <mxche2019@outlook.com>
2025-02-09 02:06:57 +0100

    udpate gp noise eval

--- benchmarking_sim/quadrotor/results_noise.py
+++ benchmarking_sim/quadrotor/results_noise.py
@@ -26,6 +26,7 @@ s = 2 # times of std
 # noise_factor = int(sys.argv[4])
 algo = sys.argv[1]
 gp_model_tag = sys.argv[2] if len(sys.argv) > 2 else ''
+noise_type = sys.argv[3] if len(sys.argv) > 3 else 'obs_noise'
 # task_description = munch.munchify({
 #     'additional': additional,
 #     'algo': algo,
@@ -42,33 +43,34 @@ noise_factor_list = [0,1,2,3,4,5,10,15,20,25,\
                      30,40,50,60,70,80,90,100]
 # noise_factor_list[0] = 1
 # for algo in ['pid', 'lqr', 'ilqr']:
-for noise_factor in noise_factor_list:
-    for start_seed in range(1, 11):
-        task_description = munch.munchify({
-            'additional': additional,
-            'algo': algo,
-            'noise_factor': noise_factor,
-            'eval_task': 'obs_noise',
-            'num_seed': 1,
-            'start_seed': start_seed,
-            # 'SYS': 'quadrotor_3D_attitude',
-            'SYS': 'quadrotor_2D_attitude', 
-            'gp_model_tag': gp_model_tag,
-            })
-        run_rollouts(task_description)
+if noise_type == 'obs':
+    for noise_factor in noise_factor_list:
+        for start_seed in range(1, 11):
+            task_description = munch.munchify({
+                'additional': additional,
+                'algo': algo,
+                'noise_factor': noise_factor,
+                'eval_task': noise_type,
+                'num_seed': 1,
+                'start_seed': start_seed,
+                # 'SYS': 'quadrotor_3D_attitude',
+                'SYS': 'quadrotor_2D_attitude', 
+                'gp_model_tag': gp_model_tag,
+                })
+            run_rollouts(task_description)
         
         
-for noise_factor in noise_factor_list:
-    for start_seed in range(1, 11):
-        task_description = munch.munchify({
-            'additional': additional,
-            'algo': algo,
-            'noise_factor': noise_factor,
-            'eval_task': 'proc_noise',
-            'num_seed': 1,
-            'start_seed': start_seed,
-            # 'SYS': 'quadrotor_3D_attitude',
-            'SYS': 'quadrotor_2D_attitude', 
-            'gp_model_tag': gp_model_tag,
-            })
-        run_rollouts(task_description)
+# for noise_factor in noise_factor_list:
+#     for start_seed in range(1, 11):
+#         task_description = munch.munchify({
+#             'additional': additional,
+#             'algo': algo,
+#             'noise_factor': noise_factor,
+#             'eval_task': 'proc_noise',
+#             'num_seed': 1,
+#             'start_seed': start_seed,
+#             # 'SYS': 'quadrotor_3D_attitude',
+#             'SYS': 'quadrotor_2D_attitude', 
+#             'gp_model_tag': gp_model_tag,
+#             })
+#         run_rollouts(task_description)
