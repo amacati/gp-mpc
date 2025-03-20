dd5ca610c83beb20db5cf565d09bf7846dd6a1eb
Martin Schuck <martin.schuck@tum.de>
2025-03-20 11:48:43 +0100

    Fix _setup_symbolic

--- safe_control_gym/mpc/mpc.py
+++ safe_control_gym/mpc/mpc.py
@@ -5,15 +5,10 @@ from pathlib import Path
 import casadi as cs
 import numpy as np
 import scipy
-import torch
+from acados_template import AcadosModel, AcadosOcp, AcadosOcpSolver
 from numpy.typing import NDArray
 
 from safe_control_gym.mpc.mpc_utils import reset_constraints
-
-try:
-    from acados_template import AcadosModel, AcadosOcp, AcadosOcpSolver
-except ImportError as e:
-    raise ImportError("Acados is not installed") from e
 
 
 class MPC:
@@ -22,42 +17,25 @@ class MPC:
     state_labels = ["x", "d_x", "y", "d_y", "z", "d_z", "phi", "theta", "d_phi", "d_theta"]
     action_labels = ["T_c", "R_c", "P_c"]
 
-    def __init__(
-        self,
-        env_func,
-        q_mpc: list,
-        r_mpc: list,
-        output_dir: Path,
-        horizon: int = 5,
-        device: str = "cpu",
-        seed: int = 0,
-        prior_info: dict | None = None,
-    ):
+    def __init__(self, env_fn, q_mpc: list, r_mpc: list, output_dir: Path, horizon: int = 5):
         """Creates task and controller.
 
         Args:
-            env_func: function to instantiate task/environment.
+            env_fn: function to instantiate task/environment. Assumes that the environment already
+                uses the correct prior information to set up the symbolic model.
             q_mpc: diagonals of state cost weight.
             r_mpc: diagonals of input/action cost weight.
             output_dir: output directory to write logs and results.
             horizon: mpc planning horizon.
             device: torch device.
-            seed: random seed.
             prior_info: prior model information.
         """
-        self.output_dir = output_dir
-        if "cuda" in device and not torch.cuda.is_available():
-            raise RuntimeError("CUDA device requested but not available.")
-        self.device = device
-        self.seed = seed
-
-        # Task.
-        env = env_func()
+        # Model parameters
+        env = env_fn()
         _, state_cnstrs, input_cnstrs = reset_constraints(env.constraints.constraints)
         assert len(state_cnstrs) == 1 and len(input_cnstrs) == 1
-
-        # Model parameters
-        env._setup_symbolic(prior_prop=prior_info.get("prior_prop", {}))
+        # env._setup_symbolic ignores prior_info about identified model parameters
+        env._setup_symbolic(prior_prop=env.INERTIAL_PROP)
         self.model = env.symbolic
         self.t_symbolic_fn = env.T_mapping_func  # Required for GP_MPC. TODO: Remove
         self.dt = self.model.dt
@@ -65,22 +43,23 @@ class MPC:
         self.traj = env.X_GOAL.T
         self.traj_step = 0
         self.u_ref = np.repeat(env.U_GOAL[:, None], self.T, axis=-1)
+        env.close()
         assert len(q_mpc) == self.model.nx
         assert len(r_mpc) == self.model.nu
         Q = np.diag(q_mpc)
         R = np.diag(r_mpc)
 
+        self.output_dir = output_dir
         acados_model = self.setup_acados_model()
         ocp = self.setup_acados_optimizer(acados_model, Q, R)
         state_cnstr, input_cnstr = (state_cnstrs[0](ocp.model.x), input_cnstrs[0](ocp.model.u))
         ocp = self.setup_acados_constraints(ocp, state_cnstr, input_cnstr)
-        json_file = self.output_dir / "acados_ocp.json"
-        self.acados_ocp_solver = AcadosOcpSolver(ocp, json_file=str(json_file), verbose=False)
-        env.close()
+        json_file = output_dir / "acados_ocp.json"
+        self.acados_solver = AcadosOcpSolver(ocp, json_file=str(json_file), verbose=False)
 
     def reset(self):
         """Prepares for training or evaluation."""
-        self.acados_ocp_solver.reset()
+        self.acados_solver.reset()
         self.traj_step = 0
 
     def setup_acados_model(self) -> AcadosModel:
@@ -111,8 +90,7 @@ class MPC:
         return acados_model
 
     def setup_acados_optimizer(self, model: AcadosModel, Q: NDArray, R: NDArray) -> AcadosOcp:
-        """Sets up nonlinear optimization problem."""
-        # Create ocp object to formulate the OCP
+        """Set up the nonlinear optimization problem in Acados."""
         ocp = AcadosOcp()
         ocp.model = model
         nx, nu = self.model.nx, self.model.nu
@@ -129,11 +107,9 @@ class MPC:
         ocp.cost.Vu = np.zeros((ny, nu))
         ocp.cost.Vu[nx : (nx + nu), :nu] = np.eye(nu)
         ocp.cost.Vx_e = np.eye(nx)
-        # Placeholder y_ref and y_ref_e. We update these in select_action.
+        # Placeholder y_ref, y_ref_e and initial state constraint. We update yref in select_action.
         ocp.cost.yref = np.zeros(ny)
         ocp.cost.yref_e = np.zeros(ny_e)
-
-        # Placeholder initial state constraint
         ocp.constraints.x0 = np.zeros(nx)
 
         # set up solver options
@@ -188,26 +164,22 @@ class MPC:
     def select_action(self, obs: NDArray) -> NDArray:
         """Solves the nonlinear mpc problem to get next action."""
         # Set initial condition (0-th state)
-        self.acados_ocp_solver.set(0, "lbx", obs)
-        self.acados_ocp_solver.set(0, "ubx", obs)
-        # Skip warmstart for simplicity. Acados has built-in warmstart (?)
+        self.acados_solver.set(0, "lbx", obs)
+        self.acados_solver.set(0, "ubx", obs)
         goal_states = self.reference_trajectory()
         self.traj_step += 1
 
-        # TODO: Can this be replaced with a single set call?
         y_ref = np.concatenate((goal_states[:, :-1], self.u_ref), axis=0)
         for idx in range(self.T):
-            self.acados_ocp_solver.set(idx, "yref", y_ref[:, idx])
+            self.acados_solver.set(idx, "yref", y_ref[:, idx])
         y_ref_e = goal_states[:, -1]
-        self.acados_ocp_solver.set(self.T, "yref", y_ref_e)
-        # Solve the optimization problem
-        self.acados_ocp_solver.solve()
-        return self.acados_ocp_solver.get(0, "u")
+        self.acados_solver.set(self.T, "yref", y_ref_e)
+        self.acados_solver.solve()
+        return self.acados_solver.get(0, "u")
 
     def reference_trajectory(self):
         """Construct reference states along mpc horizon.(nx, T+1)."""
-        # We append the T+1 states of the trajectory to the goal_states such that the vel states
-        # won't drop at the end of an episode
+        # We append the T+1 states of the trajectory to the goal_states to avoid discontinuities
         extended_traj = np.concatenate([self.traj, self.traj[:, : self.T + 1]], axis=1)
         # Slice trajectory for horizon steps, if not long enough, repeat last state.
         start = min(self.traj_step, extended_traj.shape[-1])
